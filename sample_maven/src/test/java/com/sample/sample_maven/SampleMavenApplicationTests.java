490a4dc7f41d2c174e41dc447e9d5b3e4d80f79d
supremehappy <spuremehappy@aegisep.com>
2022-03-24 08:39:53 +0900

    maven and gradle
    maven project 와 gradle project 구분하여 업로드

---
+++ sample_maven/src/test/java/com/sample/sample_maven/SampleMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.sample_maven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
